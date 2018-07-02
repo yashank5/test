43235a31941294e8409ad630e4a3f21eef855fe2
Sharma, Yashank (INFOSYS) <yashank.sharma@bp.com>
2018-07-02 12:25:03 +0100

    testing the code save

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dont_Say_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello...");
+            Console.Write("testing..");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Press Any Key to exit");
+            Console.ReadKey();
+         }
+    }
+}
